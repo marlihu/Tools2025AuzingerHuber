5fbd24e276b3a5b3ec102957e5bd20b6926c0de7
ShyMauzi <florian.a@gmx.at>
2025-05-26 12:14:59 +0200

    Added new line of code in existing method

--- auzinger_huber_tools/test_tools/src/Main.java
+++ auzinger_huber_tools/test_tools/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Strg+F8.
             System.out.println("i = " + i);
+            String newString = "New line of code";
         }
     }
 }
